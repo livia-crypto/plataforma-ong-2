1bd5c143a245d83f63989f94505e68ba2ac5491b
Lívia Oliveira <livia0306.lom@gmail.com>
2025-11-03 15:30:44 -0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+// Menu hambúrguer responsivo
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav ul');
+
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
